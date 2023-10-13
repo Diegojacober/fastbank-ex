36f2e53d1245e6b977887b3a1ad6226d5d81a6d0
Diego <diegoalencar.jacober@gmail.ocm>
2023-10-13 11:45:59 -0300

    Criando user views e configurando as rotas

---
+++ user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from user import views
+
+app_name = 'user'
+
+url_patterns = [
+    path('create', views.CreateUserView.as_view(), name='create-user'),
+    path('me/', views.ManagerUserAPiView.as_view(), name='me')
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+"""
+Views for the user API.
+"""
+from rest_framework.response import Response
+from rest_framework import (
+    status,
+    generics
+)
+from rest_framework_simplejwt import authentication as authenticationJWT
+from user.serializers import UserSerializer
+from user.permissions import IsCreationOrIsAuthenticated
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
+
+
+class ManagerUserAPiView(generics.RetrieveUpdateAPIView, generics.CreateAPIView):
+    """Manage for the users"""
+    serializer_class = UserSerializer
+    authentication_classes = [authenticationJWT.JWTAuthentication]
+    permission_classes = [IsCreationOrIsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return a user."""
+        return self.request.user
