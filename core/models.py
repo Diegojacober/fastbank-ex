a71c23150174db7b78b5bc452ca36fbf67f0a1d9
Diego <diegoalencar.jacober@gmail.ocm>
2023-10-13 10:51:52 -0300

    Criando user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,53 @@
+"""
+Models de toda a aplicaçãp
+"""
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(self, email, password=None, **extra_fiels):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError("User must be an email address")
+
+        user = self.model(email=self.normalize_email(email), **extra_fiels)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create, save and return a new super user."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in system"""
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255, null=False)
+    last_name = models.CharField(max_length=255, null=False)
+    cpf = models.CharField(max_length=11, unique=True, null=False)
+    # url_imagem = models.ImageField(null=True, upload_to=user_image_field)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
