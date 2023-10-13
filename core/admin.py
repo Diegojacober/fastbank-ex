cad606f15f46f8d52ce1578298dcb90679983d80
Diego <diegoalencar.jacober@gmail.ocm>
2023-10-13 11:01:51 -0300

    Configurando page admin django

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,48 @@
+"""
+File admin django
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for user"""
+    ordering = ['id']
+    list_display = ['id', 'first_name', 'last_name', 'cpf']
+    fieldsets = (
+        (None, {'fields': ('email', 'password',)}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'cpf', )}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login', 'created_at',)})
+    )
+    readonly_fields = ['last_login',  'created_at']
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
